45e23b118a65516c170eaca068a6ecfb528718c7
RailsRubyX <rubyproject@yahoo.com>
2019-05-12 18:36:38 +0300

    Adding bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
+	before_action :set_post, only: [:edit, :update, :show, :destroy]
 
 	def index
 		@posts = Post.all.order('created_at DESC')
@@ -20,11 +21,10 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
+		
 	end
 	
 	def update
-		@post = Post.find(params[:id])
 
 		if @post.update(params[:post].permit(:title, :body))
 			redirect_to @post
@@ -34,17 +34,21 @@ class PostsController < ApplicationController
 	end	
 
 	def show
-		@post = Post.find(params[:id])
+		
 	end	
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 
 		redirect_to root_path
 	end	
 
 	private
+
+		def set_post
+			@post = Post.find(params[:id])
+		end	
+
 		def post_params
 			params.require(:post).permit(:title, :body)
 		end	
